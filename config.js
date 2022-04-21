bea28e766887d8eac6521f7855552504b5827f00
nausicaaEmberdan <101249789+nausicaaEmberdan@users.noreply.github.com>
2022-04-21 11:17:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'PEEK A BOO',
+    introDesc: `Bất ngờ chưa mọi người
+    Chỉ một vài bước nhỏ nữa thôi sẽ có bất ngờ nhé`,
+    btnIntro: 'hihi',
+    title: 'SNEAK PEEK',
+    desc: 'Ảo ma canada chọn đi nào',
+    btnYes: 'Giải cứu thế giới',
+    btnNo: 'Không, về nhà nằm ngủ ôm áo CLB :3',
+    question:'UwU yêu CLB là tốt nhưng phải yêu thế giới hơn nha!',
+    btnReply: 'Vâng ạ\(^w^)/',
+    reply: 'Chuẩn bị tinh thần giải cứu thế giới khỏi tippy',
+    mess: 'Chuẩn rồi đấy, một lựa chọn đúng đắn',
+    messDesc: 'Vậy thì mọi người đã sẵn sàng chưa nào?',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://forms.gle/SQd7Fhn8JwZnWc2V9' 
+}
